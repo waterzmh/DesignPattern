489c8398e7054ec812a9a251b332d7ef92de0b56
water <903936820@qq.com>
2018-08-26 15:11:52 +0800

    master modified merge
    (cherry picked from commit 5dbe7858bb1c20299d328cf3c3573171cef0b67c)

--- src/create/singleton/Test.java
+++ src/create/singleton/Test.java
@@ -7,30 +7,34 @@ import java.util.logging.Logger;
  * Created by Administrator on 2017/12/7.
  */
 public class Test {
-    private Logger logger=Logger.getLogger("singleton");
+    private Logger logger = Logger.getLogger("singleton");
+
     @org.junit.Test
-    public void create(){
-       Man.INSTANCE.getMan();
-       Man.INSTANCE.setName("zmh44444");
-       logger.log(Level.INFO,Man.INSTANCE.getName());
-       logger.log(Level.INFO, String.valueOf(Man.INSTANCE.getAge()));
+    public void create() {
+        Man.INSTANCE.getMan();
+        Man.INSTANCE.setName("zmh44444");
+        logger.log(Level.INFO, Man.INSTANCE.getName());
+        logger.log(Level.INFO, String.valueOf(Man.INSTANCE.getAge()));
     }
+
     @org.junit.Test
-    public void  create2(){
+    public void create2() {
         Hungry.getHungry().setName("zmh");
         Hungry.getHungry().setAge(1);
-        logger.log(Level.INFO,Hungry.getHungry().getName());
-        logger.log(Level.INFO,String.valueOf(Hungry.getHungry().getAge()));
+        logger.log(Level.INFO, Hungry.getHungry().getName());
+        logger.log(Level.INFO, String.valueOf(Hungry.getHungry().getAge()));
     }
+
     @org.junit.Test
-    public void  create3(){
+    public void create3() {
         Hungry.getHungry().setName("zmh2");
         Hungry.getHungry().setAge(2);
-        logger.log(Level.INFO,Hungry.getHungry().getName());
-        logger.log(Level.INFO,String.valueOf(Hungry.getHungry().getAge()));
+        logger.log(Level.INFO, Hungry.getHungry().getName());
+        logger.log(Level.INFO, String.valueOf(Hungry.getHungry().getAge()));
     }
+
     @org.junit.Test
-    public void branch1(){
+    public void branch1() {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
@@ -38,14 +42,16 @@ public class Test {
         System.out.println(5);
         System.out.println(7);
     }
+
     @org.junit.Test
-    public void masterBranch1(){
+    public void masterBranch1() {
         System.out.println("master branch update 1");
         System.out.println("master branch update 2");
         System.out.println("master branch update 3");
     }
+
     @org.junit.Test
-    public void master(){
+    public void master() {
         System.out.println("master update 1");
     }
 }
